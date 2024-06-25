5762d3a0fd57cf3298b16de06f0f54f80ba99476
Alessia Vanni <alevanni19@gmail.com>
2024-06-25 12:42:05 +0200

    Created the controller files
    I created the controller files, I updated the routing and added the option to delete data

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,3 +1,30 @@
+<h1>Items</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th>Naam</th>
+            <th>Beschrijving</th>
+            <th>Acties</th>
+        </tr>
+    </thead>
+    <tbody>
+     @foreach($items as $item)
+            <tr>
+                <td>{{ $item->name }}</td>
+                <td>{{ $item->description }}</td>
+                <td>
+                  <form action="{{ route('items.destroy', $item->id) }}" method="POST">
+                     @csrf
+                     @method('DELETE')
+                     <button type="submit">Verwijderen</button>
+                  </form>
+</td>
+             </tr>
+      @endforeach
+    </tbody>
+</table>
+
 @extends('layouts.app')
 
 @section('title', 'Page Title')
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,18 +13,16 @@ Route::get('/hello', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
-Route::get('/items', function () {
-    return view('items.index');
 
-})->name('items.index');
-Route::get('/items/create', function () {
-    return view('items.create');
-})->name('items.create');
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
+
 Route::post('/items', function () {})->name('items.store');
 Route::get('/items/{item}', function () {})->name('items.show');
 Route::get('/items/{item}/edit', function () {})->name('items.edit');
 Route::put('/items/{item}', function () {})->name('items.update');
-Route::delete('/items/{item}', function () {})->name('items.destroy');
+Route::delete('/items/{id}', [ItemController::class, 'destroy'])->name('items.destroy');
 
 // We voegen ook een redirect toe aan de routes die de hoofdpagina doorverwijst naar de '/items' route
 Route::redirect('/', '/items');
